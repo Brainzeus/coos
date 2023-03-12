f20cc80107f4ff807f3b2b2347ff32914c374621
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-03-13 01:43:05 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -7,7 +7,8 @@ from bitcoinlib.wallets import Wallet
 address = "19D8wvqt8iSSXe3AmBZeLZRyEKS4EXPnZ3"
 
 # Transformă adresa în formatul necesar pentru utilizarea în biblioteca bitcoinlib
-address_bytes = bytes.fromhex(addr_to_pubkeyhash(address).decode())
+address_bytes = bytes.fromhex(addr_to_pubkeyhash(address.encode('ISO-8859-1')))
+
 
 
 # Creează o cheie privată aleatorie utilizând biblioteca bitcoinlib
