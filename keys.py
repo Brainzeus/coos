bfd7ad83374ef95d41b5f2e03b2c48cc4df0f35e
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-03-13 02:24:55 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,17 +1,27 @@
-import bitcoin
-import ecdsa
+import requests
+import bitcoinlib.keys as keys
+from bitcoinlib.encoding import to_bytes, to_hex
 
-# definim adresa Bitcoin
-addr = '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa'
+# Introduceți adresa Bitcoin pentru care doriți să recuperați semnătura ECDSA R, S, Z
+address = "113324vM6NBar2q72w6iDCdQvPnPQw8Tvw"
 
-# convertim adresa in format binar
-addr_bin = bitcoin.b58check_to_bin(addr)
+# Descărcați tranzacțiile asociate cu adresa utilizând API-ul blockchain.info
+txs = requests.get(f"https://blockchain.info/address/{address}?format=json").json()["txs"]
 
-# generam cheia publica si punctul de pe curba SECP256k1
-vk = ecdsa.VerifyingKey.from_string(addr_bin[1:], curve=ecdsa.SECP256k1)
-x, y = vk.pubkey.point.x(), vk.pubkey.point.y()
-
-# afisam coordonatele in format zecimal
-print(f'Coordonatele pentru adresa {addr} sunt:')
-print(f'x = {x}')
-print(f'y = {y}')
+# Recuperați semnăturile pentru fiecare tranzacție
+for tx in txs:
+    raw_tx = requests.get(f"https://blockchain.info/rawtx/{tx['hash']}").json()
+    for input in raw_tx["inputs"]:
+        if input["prev_out"]["addr"] == address:
+            # Recuperați semnătura R, S, Z din scriptSig
+            scriptSig = input["script"]
+            sig = keys.parse_scriptSig(scriptSig)
+            sig_der = sig[0]
+            sig_hash_type = sig[1]
+            r = to_hex(to_bytes(sig_der[0], 32))
+            s = to_hex(to_bytes(sig_der[1], 32))
+            z = to_hex(to_bytes(sig_hash_type, 1))
+            print(f"Tranzacția: {tx['hash']}")
+            print(f"Semnătură R: {r}")
+            print(f"Semnătură S: {s}")
+            print(f"Semnătură Z: {z}")
